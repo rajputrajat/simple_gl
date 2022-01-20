051f27f502d0626152f45ed360634edef0d096e6
Rajat Rajput <rajputrajat@gmail.com>
2022-01-21 00:16:54 +0530

    include io errors

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,9 @@
 use glfw::InitError;
+use std::io;
 
 #[derive(Debug)]
 pub enum CustomError {
+    Io(io::Error),
     Glfw(InitError),
     Other(String),
 }
@@ -9,6 +11,12 @@ pub enum CustomError {
 impl From<InitError> for CustomError {
     fn from(e: InitError) -> Self {
         CustomError::Glfw(e)
+    }
+}
+
+impl From<io::Error> for CustomError {
+    fn from(e: io::Error) -> Self {
+        CustomError::Io(e)
     }
 }
 
